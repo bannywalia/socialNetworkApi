bba36581601023b0b4b85179beafbe4a7c1391b7
Bannywalia <Banny.walia07@gmail.com>
2023-01-21 14:05:50 -0500

    added cru ops

--- userManagement/routes/user-routes.js
+++ userManagement/routes/user-routes.js
@@ -1,1 +1,15 @@
-const
+const express = require("express");
+const {
+  getAllUsers,
+  addUser,
+  updateUser,
+} = require("../controller/user-controller");
+
+const router = express.Router();
+
+router.get("/", getAllUsers);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id");
+
+module.exports = router;
