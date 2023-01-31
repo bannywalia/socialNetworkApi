17eb6ec318e61f31a5d6f4f47dee2712fecef14a
Bannywalia <Banny.walia07@gmail.com>
2023-01-31 14:26:38 -0500

    added user and thoughts

---
+++ userManagement/routes/api/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+//set up GET and POST routes for /api/thoughts/
+router.route("/").get(getAllThoughts).post(addThought);
+
+//set up GET, PUT, and DELETE routes for /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+//set up POST and DELETE routes for /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
+module.exports = router;
---
+++ userManagement/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+//set up GET and POST routes for /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+//set up GET, PUT, and DELETE for /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+//set up POST and DELETE routes for /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
