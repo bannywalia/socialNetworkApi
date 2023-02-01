785815ed8a16bc81ef91dfb92829955b8aa79324
Bannywalia <Banny.walia07@gmail.com>
2023-01-31 21:00:28 -0500

    updated server

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cat-chat", {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/socialnetwork",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //enable mongo logging
 mongoose.set("debug", true);
